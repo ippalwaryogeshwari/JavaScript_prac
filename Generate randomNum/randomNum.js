88a259c8a15de082e2bb5d47bdc34cbe6c18e225
yogeshwari <yogeshwariippalwar@gmail.com>
2026-09-23 05:02:23 +0530

    to changing into game

--- Generate randomNum/randomNum.js
+++
@@ -1,8 +0,0 @@
-console.log("generating numbers form 1 to 100.")
-let num = Math.random();
-num = Math.floor(Math.random()*100)+1;
-// num = num*10;
-// num = Math.floor(num);
-// num = num+1;
-console.log(num);
-
