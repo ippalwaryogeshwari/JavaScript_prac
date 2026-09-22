3d20c0c1f3ca666bb0fce67078c9a7b79e4c8469
yogeshwari <yogeshwariippalwar@gmail.com>
2026-09-23 04:27:37 +0530

    add generate number file

---
+++ Generate randomNum/randomNum.js
@@ -0,0 +1,8 @@
+console.log("generating numbers form 1 to 100.")
+let num = Math.random();
+num = Math.floor(Math.random()*100)+1;
+// num = num*10;
+// num = Math.floor(num);
+// num = num+1;
+console.log(num);
+
