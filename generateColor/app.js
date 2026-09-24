ff25522574106c429787233dfacee3d6917c0c72
yogeshwari <yogeshwariippalwar@gmail.com>
2026-09-24 08:11:46 +0530

    improvement code

--- generateColor/app.js
+++ generateColor/app.js
@@ -1,2 +1,8 @@
 let red = Math.floor(Math.random()*255)+1;
-console.log(red);
+let green = Math.floor(Math.random()*255)+1;
+let blue = Math.floor(Math.random()*255)+1;
+
+let container = document.querySelector('.container');
+
+container = `(${red},${green},${blue})`;
+console.log(container);
